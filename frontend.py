1b925e12570bcd0ed8e0c009ab63ff69819e19a0
Ldpw0xi1m1d <dimas.batrakov.1995@bk.ru>
2024-09-23 16:54:19 +0300

    2nd commit

--- frontend.py
+++ frontend.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from backend import backClick
+from backend import backGetData
 
 def onClick():
     text1 = message.get()
@@ -7,11 +8,15 @@ def onClick():
     print(text1)
     message_entry.delete(0, END)
 
+def onClickGet():
+    textFromFile = backGetData()
+    lbl.config(text=textFromFile)
+
 
 if __name__ == '__main__':
     root = Tk()
     root.title("My First App")
-    root.geometry("200x100")
+    root.geometry("300x150")
 
     message = StringVar()
 
@@ -20,5 +25,11 @@ if __name__ == '__main__':
 
     message_button = Button(text="Отправить", command=onClick)
     message_button.place(relx=.5, rely=.5, anchor="c")
+
+    data_button = Button(text="Получить", command=onClickGet)
+    data_button.place(relx=.5, rely=.3, anchor="c")
+
+    lbl = Label(root, text="Привет")
+    lbl.place(relx=.13, rely=.13, anchor="c")
 
     root.mainloop()
